49d93483fe0d910be28f0ad41cd29077a7adea7f
Niels Van Och <niels.vanoch@gmail.com>
2014-01-24 10:03:49 +0100

    Update datetime call

--- djprogress/__init__.py
+++ djprogress/__init__.py
@@ -7,7 +7,7 @@ import sys
 try:
     from django.utils.timezone import now
 except ImportError:
-    from datetime.datetime import now
+    now = datetime.datetime.now
 
 from django.views.debug import ExceptionReporter
 
@@ -26,7 +26,7 @@ def get_git_version():
         git_info = subprocess.Popen(['git', '--git-dir=%s' % git_dir, 'log', '--pretty=%ct %h', '-1'], stdout=subprocess.PIPE).communicate()[0].split()
         git_time = datetime.datetime.fromtimestamp(float(git_info[0]))
     except Exception:
-        git_time = datetime.datetime.now()
+        git_time = now()
         git_info = ('', '0000000')
     return git_time.strftime('%Y.%m.%d') + '.' + git_info[1]
 
@@ -37,7 +37,7 @@ VERSION = __version__
 class ProgressException(Exception):
     def __init__(self, description):
         self.description = description
-    
+
     def __unicode__(self):
         return self.description
 
@@ -49,28 +49,28 @@ tls = threading.local()
 def with_progress(collection, name=None, count=-1):
     """
     This is a generator for keeping track of the progress of long running tasks.
-    
+
     Example:
-    
+
     >>> for item in with_progress(list_of_items, name='My hardcore processing action'):
     >>>     # heavy processing action with item
-    
+
     name can be anything, but is limited to 64 characters.
-    
+
     In the Django Admin, you can see an overview of all running processes with their
     progress and an estimated time of arrival/completion. This information is updated
     at a minimum interval of 5 seconds.
     """
     from djprogress.models import Progress
-    
+
     if not name:
         raise ProgressException('This with_progress call has no name')
-    
+
     count = count if count > -1 else len(collection)
     start_ts = now()
     last_updated = start_ts
     items_since_retarget = 0
-    
+
     ### Keep track of parent progresses using threading.local
     if not hasattr(tls, 'djprogress__stack'):
         tls.djprogress__stack = []
@@ -82,9 +82,9 @@ def with_progress(collection, name=None, count=-1):
             parent_progresses = Progress.objects.filter(pk=tls.djprogress__stack[-1])
         if parent_progresses:
             parent_progress = parent_progresses[0]
-    
+
     progress = Progress.objects.create(name=name, total=count, parent=parent_progress)
-    
+
     tls.djprogress__stack.append(progress.pk)
     for i, item in enumerate(collection):
         yield item
