cb9b34f9ee5a7d4652d980cf170ea8b612c5ec0f
Jakub Paczkowski <jakub@paczkowski.eu>
2015-02-06 10:59:48 +0100

    migrations hell, woohoo!

--- djprogress/migrations/0001_initial.py
+++ djprogress/migrations/0001_initial.py
@@ -1,41 +1,31 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        
-        # Adding model 'Progress'
-        db.create_table('djprogress_progress', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=64, db_index=True)),
-            ('current', self.gf('django.db.models.fields.IntegerField')(default=0)),
-            ('total', self.gf('django.db.models.fields.IntegerField')(default=100)),
-            ('eta', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
-            ('last_updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, db_index=True, blank=True)),
-        ))
-        db.send_create_signal('djprogress', ['Progress'])
+from django.db import models, migrations
 
 
-    def backwards(self, orm):
-        
-        # Deleting model 'Progress'
-        db.delete_table('djprogress_progress')
+class Migration(migrations.Migration):
 
+    dependencies = [
+    ]
 
-    models = {
-        'djprogress.progress': {
-            'Meta': {'object_name': 'Progress'},
-            'current': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
-            'eta': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'db_index': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'db_index': 'True'}),
-            'total': ('django.db.models.fields.IntegerField', [], {'default': '100'})
-        }
-    }
-
-    complete_apps = ['djprogress']
+    operations = [
+        migrations.CreateModel(
+            name='Progress',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=64, db_index=True)),
+                ('current', models.IntegerField(default=0)),
+                ('total', models.IntegerField(default=100)),
+                ('eta', models.DateTimeField(null=True, blank=True)),
+                ('last_updated', models.DateTimeField(auto_now=True, db_index=True)),
+                ('exception', models.TextField(blank=True)),
+                ('parent', models.ForeignKey(related_name='children', blank=True, to='djprogress.Progress', null=True)),
+            ],
+            options={
+                'verbose_name': 'progress',
+                'verbose_name_plural': 'progresses',
+            },
+            bases=(models.Model,),
+        ),
+    ]
