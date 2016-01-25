7499183a330a207b41687cd605d3a2d4a2a38c57
Linsey Raymaekers <linsey.raymaekers@vikingco.com>
2016-01-25 10:45:41 +0100

    Added mandatory on_delete argument to ForeignKey/OneToOneField

--- djprogress/models.py
+++ djprogress/models.py
@@ -8,7 +8,7 @@ class Progress(models.Model):
     total = models.IntegerField(default=100)
     eta = models.DateTimeField(null=True, blank=True)
     last_updated = models.DateTimeField(auto_now=True, db_index=True)
-    parent = models.ForeignKey('Progress', blank=True, null=True, related_name='children')
+    parent = models.ForeignKey('Progress', on_delete=models.CASCADE, blank=True, null=True, related_name='children')
     exception = models.TextField(blank=True)
     
     class Meta:
