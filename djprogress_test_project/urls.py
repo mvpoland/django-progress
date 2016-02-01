6ac529ab187537cf5fc36eeed968067c255eec1c
Linsey Raymaekers <linsey.raymaekers@vikingco.com>
2016-02-01 13:46:49 +0100

    admin.site.urls is now passed directly to url()

--- djprogress_test_project/urls.py
+++ djprogress_test_project/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^progress/', include('djprogress.urls')),
 )
