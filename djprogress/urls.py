818e0307fdd3bb8e1a2cec60d828efbf4d491705
meverone <meverone@gmail.com>
2017-09-13 09:53:41 +0200

    MVPL-3379: Clear RemovedInDjango110Warning

--- djprogress/urls.py
+++ djprogress/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url
 
-urlpatterns = patterns('djprogress.views',
-    url(r'^$', 'overview', name='djprogress_overview'),
-    url(r'^exception/(?P<progress_id>\d+)/$', 'show_exception', name='djprogress_show_exception'),
-    url(r'^resolve/(?P<progress_id>\d+)/$', 'resolve', name='djprogress_resolve'),
-    url(r'^api/get/$', 'api_get_progress', name='djprogress_api_get'),
-)
+from .views import api_get_progress, overview, resolve, show_exception
+
+urlpatterns = [
+    url(r'^$', overview, name='djprogress_overview'),
+    url(r'^exception/(?P<progress_id>\d+)/$', show_exception, name='djprogress_show_exception'),
+    url(r'^resolve/(?P<progress_id>\d+)/$', resolve, name='djprogress_resolve'),
+    url(r'^api/get/$', api_get_progress, name='djprogress_api_get'),
+]
